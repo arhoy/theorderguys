44dc641eea82641781c401d90bb0907ece375bf4
arhoy <you@example.com>
2020-04-13 12:32:43 -0600

    added butter cms

--- src/components/layouts/Footer/FooterComponents/FooterBottom.js
+++ src/components/layouts/Footer/FooterComponents/FooterBottom.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 import { A } from '../../../reusableStyles/typography/Typography';
 
 const Container = styled.div`
@@ -22,24 +24,32 @@ const ExternalLink = styled(A)`
   color: ${props => props.theme.colors.primary};
 `;
 
+const StyledImage = styled(Img)`
+  width: 14rem;
+`;
+
 const Attriubtion = styled.div`
   display: flex;
   flex-wrap: wrap;
   flex-direction: column;
+  justify-content: space-between;
   justify-content: center;
   font-size: 1.3rem;
+  & .linkcontainer {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    max-width: 50rem;
+    padding: 0 1rem;
+  }
   & h6 {
     font-size: 1.4rem;
     text-align: center;
     opacity: 0.6;
     font-weight: 400;
   }
-  justify-content: space-between;
-  & .linkcontainer {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-  }
+
   & ${A} {
     margin-right: 4px;
     text-align: center;
@@ -50,6 +60,18 @@ const Attriubtion = styled.div`
 `;
 
 export const FooterBottom = () => {
+  const data = useStaticQuery(graphql`
+    {
+      butter: file(relativePath: { eq: "footer/butter-cms.png" }) {
+        childImageSharp {
+          fluid(quality: 100, maxWidth: 150) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+    }
+  `);
+  console.log('data is', data);
   return (
     <Container>
       <Creator>
@@ -88,6 +110,14 @@ export const FooterBottom = () => {
           >
             React-Icons
           </A>
+          <A
+            href="https://replain.cc/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            RePlain
+          </A>
+          <StyledImage fluid={data.butter.childImageSharp.fluid} />
         </div>
       </Attriubtion>
     </Container>
